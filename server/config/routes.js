0ce85664448bf2281c8226d1ab3fb4a6293d3e10
Anthony Renwick <renwickman@gmail.com>
2020-04-02 14:28:41 -0500

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,15 @@
+const note = require("../controllers/notes.js");
+
+module.exports = function(app){
+
+    app.get("/notes", (req, res)=> {
+        console.log('in notes')
+        note.index (req, res);
+        })
+
+    app.post("/notes", (req, res)=> {
+        console.log('posting notes')
+        note.create (req, res);
+    })
+
+};
---
+++ server/controllers/notes.js
@@ -0,0 +1,30 @@
+const Note = require('../models/note.js');
+
+module.exports = {
+
+    index: function(req, res){
+        Note.find({}).sort('-created_at')
+            .then(notes => {
+                console.log(notes)
+                res.json(notes)
+            })
+            .catch(err =>{
+                console.log("We have an error!", err);
+                res.json(err)
+        })
+    },
+
+    create: function(req, res){
+        console.log('creating note')
+        let note = new Note()
+            note.post = req.body.post;
+            note.save()
+                .then(note => {
+                    res.json(note)
+                })
+                .catch(err =>{
+                    res.json(err)
+        })
+    }
+
+}
